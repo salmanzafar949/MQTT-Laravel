f0a575d7608f824009c06577248e5e697118cff7
James Walker <james@woodsandwalker.co.uk>
2020-07-02 11:07:06 +0100

    update other MqttService call

--- src/MqttClass/Mqtt.php
+++ src/MqttClass/Mqtt.php
@@ -84,7 +84,7 @@ class Mqtt
     {
         $id = empty($client_id) ?  rand(0,999) : $client_id;
 
-        $client = new MqttService($this->host,$this->port,$id, $this->cert_file, $this->debug);
+        $client = new MqttService($this->host,$this->port, $id, $this->cert_file, $this->local_cert, $this->local_pk, $this->debug);
 
         if ($client->connect(true, null, $this->username, $this->password))
         {
